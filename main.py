a944dcb435cb1183438d3fd6257d396a403f5c21
Pablo <pabloarrhdez@gmail.com>
2025-05-09 19:05:07 +0200

    modified:   main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import re
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
