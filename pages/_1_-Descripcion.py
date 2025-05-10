332129582f8a67d358c2f7343d34a69c6a912b69
Pablo <pabloarrhdez@gmail.com>
2025-05-10 16:45:33 +0200

    modified:   pages/_1_-Descripcion.py

--- pages/_1_-Descripcion.py
+++ pages/_1_-Descripcion.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.title("Descripción de los Datos")
 st.markdown("""
-
+Este conjunto de datos, previo a una posterior limpieza, unión y transformación de los mismos, simula las características que influyen en las ventas globales de un videojuego en el año que sale al mercado.
+            
+-        
             """)
