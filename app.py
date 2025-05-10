1397a87baa5a29025c947095d732c07c5077a562
Pablo <pabloarrhdez@gmail.com>
2025-05-10 15:52:06 +0200

    modified:   app.py

--- app.py
+++ app.py
@@ -2,4 +2,16 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-st.title('Modelo predictivo categórico de ventas de Videojuegos.')
+st.set_page_config(page_title="Estimador")
+st.title('🎮 Modelo predictivo categórico de ventas de Videojuegos. 👾')
+
+tab1, tab2 = st.tabs(["Resumen", "Contacto"])
+
+with tab1:
+    st.subheader("Resumen del proyecto")
+    st.write("texto sobre el resumen de tu proyecto")
+    
+with tab2:
+    st.subheader("Contacto")
+    st.write("Autor: Pablo Arrastia Hernández")
+    st.write("Email: pabloarrhdez@gmail.com")
