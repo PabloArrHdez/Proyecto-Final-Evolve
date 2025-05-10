82308288da7f5006a88cd556ac584002cfecba4b
Pablo <pabloarrhdez@gmail.com>
2025-05-10 18:35:34 +0200

    modified:   pages/_2_-Exploracion.py

--- pages/_2_-Exploracion.py
+++ pages/_2_-Exploracion.py
@@ -13,3 +13,13 @@ pegi_cateogria = st.sidebar.multiselect("PEGI", df["PEGI_categoria"].unique())
 duracion_juego = st.sidebar.multiselect("Duración", df["Duracion_juego_cat"].unique())
 precio_min, precio_max = df["Price"].min(), df["Price"].max()
 price_range = st.sidebar.slider("Precio Videojuego", precio_min, precio_max, (precio_min, precio_max))
+
+
+df_filtrado = df[
+    (df["Estado_Consola"].isin(estado_consola)) &
+    (df["Genre"].isin(genero)) &
+    (df["PEGI_categoria"].isin(pegi_cateogria)) &
+    (df["Duracion_juego_cat"].isin(duracion_juego)) &
+    (df["Price"].between(price_range[0], price_range[1]))
+]
+st.write (f"Se econtró {df_filtrado.shape[0]} videojuegos")
