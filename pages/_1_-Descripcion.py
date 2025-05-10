4920f76c5e830c4b25dd0ad264e440274a527d2b
Pablo <pabloarrhdez@gmail.com>
2025-05-10 17:15:01 +0200

    modified:   pages/_1_-Descripcion.py 	modified:   pages/_2_-Exploracion.py

--- pages/_1_-Descripcion.py
+++ pages/_1_-Descripcion.py
@@ -4,5 +4,21 @@ st.title("Descripción de los Datos")
 st.markdown("""
 Este conjunto de datos, previo a una posterior limpieza, unión y transformación de los mismos, simula las características que influyen en las ventas globales de un videojuego en el año que sale al mercado.
             
--        
+- **Platform**: Videoconsola disponible para jugar.
+- **Year**: Año de lanzamiento del videojuego.
+- **Genre**: Genero del videojuego.
+- **Publisher**: Empresa que desarrolla el juego.
+- **User_Score**: Puntuacion del cliente (0-10) en la pagina web 'Metacritic.com'
+- **User Ratings Count**: Número de veces que han puntuado.
+- **Estado_Consola**: Situación en el mercado actual de la videoconsola
+- **Price**: Precio del videojuego, en Euros (€), de salida al mercado.
+- **Price_Platform**: Precio de salida, en Euros (€), de la videoconsola al mercado.
+- **Year_Consola**: Año en el salio la videoconsola
+- **Modo Juego**: Opción del videojuego de jugar varios jugadores o es individual
+- **Años_desde_lanzamiento_consola**: Período de tiempo desde que salio al mercado la videoconsola, hasta que salio el videojuego para esa consola concreta.
+- **Precio_relativo**: precio del videojuego en relación con el precio de la consola en la que se lanza. Es decir, cuán caro es un juego comparado con el coste de la plataforma que lo soporta.
+- **PEGI_Categoria**: Clasificación oficial que establece la edad permitida que puede jugar a un videojuego en función del contenido del mismo.
+- **Duración_juego_cat**: Duración de tiempo que tiene la historia principal de un videjuego en funcion de su genero.
+- **Tipo_Saga**: Categoría que se establece para considerar si el videojuego pertenece a una Saga de videojuegos o no.
+- **Situacion_Economica**: Situación Economica Global que se vivia en los años a el lanzamiento del videojuego.                                                                
             """)
