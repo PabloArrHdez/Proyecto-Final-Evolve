017019ce5837184e6b35620cf971215c3ac6455e
Pablo <pabloarrhdez@gmail.com>
2025-05-10 17:52:42 +0200

    modified:   pages/_2_-Exploracion.py

--- pages/_2_-Exploracion.py
+++ pages/_2_-Exploracion.py
@@ -1,1 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+st.title ("Explorador de videojuegos desde 1980 hasta 2020")
+
+df = pd.read_csv('Data/video_games_sales_completo.csv')
+
+st.dataframe(df)
