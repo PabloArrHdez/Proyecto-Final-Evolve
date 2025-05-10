45d8921686b777b0256f8cc360be48456b3bae00
Pablo <pabloarrhdez@gmail.com>
2025-05-10 18:54:52 +0200

    modified:   pages/_2_-Exploracion.py

--- pages/_2_-Exploracion.py
+++ pages/_2_-Exploracion.py
@@ -9,7 +9,7 @@ st.dataframe(df)
 st.sidebar.header("Filtrar")
 estado_consola = st.sidebar.multiselect("Estado Consola", df["Estado_Consola"].unique())
 genero = st.sidebar.multiselect("Genero", df["Genre"].unique())
-pegi_cateogria = st.sidebar.multiselect("PEGI", df["PEGI_categoria"].unique())
+pegi_categoria = st.sidebar.multiselect("PEGI", df["PEGI_categoria"].unique())
 duracion_juego = st.sidebar.multiselect("Duración", df["Duracion_juego_cat"].unique())
 precio_min, precio_max = df["Price"].min(), df["Price"].max()
 price_range = st.sidebar.slider("Precio Videojuego", precio_min, precio_max, (precio_min, precio_max))
@@ -18,9 +18,10 @@ price_range = st.sidebar.slider("Precio Videojuego", precio_min, precio_max, (pr
 df_filtrado = df[
     (df["Estado_Consola"].isin(estado_consola)) &
     (df["Genre"].isin(genero)) &
-    (df["PEGI_categoria"].isin(pegi_cateogria)) &
+    (df["PEGI_categoria"].isin(pegi_categoria)) &
     (df["Duracion_juego_cat"].isin(duracion_juego)) &
     (df["Price"].between(price_range[0], price_range[1]))
-]
+    ]
+
 st.write (f"Se econtró {df_filtrado.shape[0]} videojuegos")
 st.dataframe(df_filtrado)
