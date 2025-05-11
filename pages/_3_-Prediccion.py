808cf806212d10a74581e9beed6d318dd200fdbd
Pablo <pabloarrhdez@gmail.com>
2025-05-11 10:45:49 +0200

    modified:   notebook.ipynb 	modified:   pages/_3_-Prediccion.py

--- pages/_3_-Prediccion.py
+++ pages/_3_-Prediccion.py
@@ -10,11 +10,14 @@ st.title('Predicción de Ventas de Videojuegos')
 st.markdown('Introduce los detalles del videojuego para estimar su categoría de ventas.')
 
 # Inputs visibles para el usuario
-platform = st.selectbox('Plataforma', ['PlayStation 4', 'Xbox One', 'PC', 'Nintendo Switch'])  # Ajusta tus valores reales
-genre = st.selectbox('Género', ['Action', 'Sports', 'Role-Playing', 'Shooter', 'Racing'])      # Ajusta también
-price = st.number_input('Precio del videojuego (€)', min_value=0.0, max_value=100.0, value=40.0)
+platform = st.selectbox('Plataforma', ['Nintendo Wii', 'Nintendo Entertainment System', 'Game Boy', 'Nintendo DS', 'Xbox 360', 'PlayStation 3', 'PlayStation 2', 'Super Nintendo Entertainment System', 'Game Boy Advance', 'Nintendo 3DS', 'PlayStation 4', 'Nintendo 64', 'PlayStation', 'Xbox', 'PC', 'Atari 2600', 'PlayStation Portable', 'Xbox One', 'GameCube', 'Nintendo Wii U', 'Sega Genesis', 'Dream Cast', 'PlayStation Vita', 'Sega Saturn'])  # Ajusta tus valores reales
+genre = st.selectbox('Género', ['Sports', 'Platform', 'Racing', 'Role-Playing', 'Puzzle', 'Misc', 'Shooter', 'Simulation', 'Action', 'Fighting', 'Adventure', 'Strategy'])      # Ajusta también
+price = st.number_input('Precio del videojuego (€)', min_value=29.99, max_value=69.99, value=29.99)
 modo_juego = st.selectbox('Modo de juego', ['Multijugador', 'Individual'])
-pegi = st.selectbox('Clasificación PEGI', [3, 7, 12, 16, 18])
-duracion_cat = st.selectbox('Duración estimada', ['Corta', 'Media', 'Larga'])
-
+pegi = st.selectbox('Clasificación PEGI', ['Infantil', 'Adulto', 'Adolescente'])
+duracion_cat = st.selectbox('Duración estimada', ['Corto', 'Medio', 'Largo'])
+year = st.number_input('Año del videojuego', min_value=1980, max_value=2020, value=1980)
+nota_usuario = st.number_input('Nota del usuario', min_value=0, max_value=10, value=0)
+n_votaciones = st.number_input('Nº votaciones', min_value=0, max_value=500, value=5)
+estado_consola = st.selectbox('Antigüedad Consola', ['Obsoleta', ' Consola Secundaria', ' Consola Activa'])
 
