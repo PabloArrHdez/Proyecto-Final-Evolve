d4fdd73cb48bdb99cae51ae7d3b04227c86a8084
Pablo <pabloarrhdez@gmail.com>
2025-05-12 13:19:18 +0200

    modified:   pages/_1_-Descripcion.py

--- pages/_1_-Descripcion.py
+++ pages/_1_-Descripcion.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+### IMAGEN DE FONDO ####
 st.title("Descripción de los Datos")
 st.markdown("""
 Este conjunto de datos, previo a una posterior limpieza, unión y transformación de los mismos, simula las características que influyen en las ventas globales de un videojuego en el año que sale al mercado.
