aac172f5e29469b3b11a11dfa9f3c63f5422c601
Pablo <pabloarrhdez@gmail.com>
2025-05-12 14:03:26 +0200

    modified:   pages/_1_-Descripcion.py

--- pages/_1_-Descripcion.py
+++ pages/_1_-Descripcion.py
@@ -1,11 +1,10 @@
 import streamlit as st
-
 ### IMAGEN DE FONDO ####
 st.markdown(
     f"""
     <style>
     .stApp {{
-        background-image: url("https://images.pexels.com/photos/7864443/pexels-photo-7864443.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+        background-image: url("https://images.pexels.com/photos/15763947/pexels-photo-15763947/free-photo-of-tecnologia-divertido-videojuegos-juego-de-azar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
@@ -47,11 +46,19 @@ st.markdown(
     </style>
 
     <div class="credit">
-        Foto de <a href="https://www.pexels.com/es-es/@vazhnik/" target="_blank">VAZHNIK</a> en <a href="https://www.pexels.com/es-es/" target="_blank">Pexels</a>
+        Foto de <a href="https://www.pexels.com/es-es/@hellojoshwithers/" target="_blank">Josh Withers</a> en <a href="https://www.pexels.com/es-es/" target="_blank">Pexels</a>
     </div>
     """,
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
+
+st.markdown("""
+    <style>
+    .stMarkdown, .markdown-text-container, .stText, .stTitle, .stHeader {
+        color: black !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 st.title("Descripción de los Datos")
 st.markdown("""
 Este conjunto de datos, previo a una posterior limpieza, unión y transformación de los mismos, simula las características que influyen en las ventas globales de un videojuego en el año que sale al mercado.
@@ -60,17 +67,17 @@ Este conjunto de datos, previo a una posterior limpieza, unión y transformació
 - **Year**: Año de lanzamiento del videojuego.
 - **Genre**: Genero del videojuego.
 - **Publisher**: Empresa que desarrolla el juego.
-- **User_Score**: Puntuacion del cliente (0-10) en la pagina web 'Metacritic.com'
+- **User_Score**: Puntuacion del cliente (0-10) en la pagina web 'Metacritic.com'.
 - **User Ratings Count**: Número de veces que han puntuado.
-- **Estado_Consola**: Situación en el mercado actual de la videoconsola
+- **Estado_Consola**: Situación en el mercado actual de la videoconsola.
 - **Price**: Precio del videojuego, en Euros (€), de salida al mercado.
 - **Price_Platform**: Precio de salida, en Euros (€), de la videoconsola al mercado.
-- **Year_Consola**: Año en el salio la videoconsola
-- **Modo Juego**: Opción del videojuego de jugar varios jugadores o es individual
+- **Year_Consola**: Año en el salio la videoconsola.
+- **Modo Juego**: Opción del videojuego de jugar varios jugadores o es individual.
 - **Años_desde_lanzamiento_consola**: Período de tiempo desde que salio al mercado la videoconsola, hasta que salio el videojuego para esa consola concreta.
 - **Precio_relativo**: precio del videojuego en relación con el precio de la consola en la que se lanza. Es decir, cuán caro es un juego comparado con el coste de la plataforma que lo soporta.
 - **PEGI_Categoria**: Clasificación oficial que establece la edad permitida que puede jugar a un videojuego en función del contenido del mismo.
 - **Duración_juego_cat**: Duración de tiempo que tiene la historia principal de un videjuego en funcion de su genero.
 - **Tipo_Saga**: Categoría que se establece para considerar si el videojuego pertenece a una Saga de videojuegos o no.
-- **Situacion_Economica**: Situación Economica Global que se vivia en los años a el lanzamiento del videojuego.                                                                
+- **Situacion_Economica**: Situación Economica Global que se vivia en los años a el lanzamiento del videojuego.
             """)
