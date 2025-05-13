b1794996228e9546427feab0461497567868fa14
Pablo <pabloarrhdez@gmail.com>
2025-05-13 12:29:24 +0200

    modified:   pages/_1_-Descripcion.py

--- pages/_1_-Descripcion.py
+++ pages/_1_-Descripcion.py
@@ -1,5 +1,46 @@
 import streamlit as st
 
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url("https://images.pexels.com/photos/15763947/pexels-photo-15763947/free-photo-of-tecnologia-divertido-videojuegos-juego-de-azar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    h1, h2, h3, h4, h5, h6, p, div, span {{
+        color: black !important;
+    }}
+        section[data-testid="stSidebar"] > div {{
+        background-color: rgba(0, 0, 0, 0.5);
+    }}
+        /* Créditos de la imagen */
+    .credit {{
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+        font-size: 13px;
+        background-color: rgba(0, 0, 0, 0.6);
+        padding: 6px 10px;
+        border-radius: 8px;
+        color: white;
+        border: 1px solid white;
+        box-shadow: 0 0 6px rgba(255, 255, 255, 0.4);
+        z-index: 100;
+    }}
+        .credit a {{
+        color: white;
+        text-decoration: underline;
+    }}
+    </style>
+
+    <div class="credit">
+        Foto de <a href="https://www.pexels.com/es-es/@hellojoshwithers/" target="_blank">Josh Withers</a> en <a href="https://www.pexels.com/es-es/" target="_blank">Pexels</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 st.title("Descripción de los Datos")
 st.markdown("""
 Este conjunto de datos, previo a una posterior limpieza, unión y transformación de los mismos, simula las características que influyen en las ventas globales de un videojuego en el año que sale al mercado.
