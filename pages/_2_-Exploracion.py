dbdf13225e7e812e278cfad5294847a03179ab4d
Pablo <pabloarrhdez@gmail.com>
2025-05-13 13:06:42 +0200

    modified:   pages/_2_-Exploracion.py

--- pages/_2_-Exploracion.py
+++ pages/_2_-Exploracion.py
@@ -1,6 +1,48 @@
 import streamlit as st
 import pandas as pd
 
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url("https://images.pexels.com/photos/4522998/pexels-photo-4522998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    h1, h2, h3, h4, h5, h6, p, div, span {{
+        color: black !important;
+    }}
+        section[data-testid="stSidebar"] > div {{
+        background-color: rgba(255, 255, 255, 0.8);
+    }}
+        /* Créditos de la imagen */
+    .credit {{
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+        font-size: 13px;
+        background-color: rgba(245, 245, 245, 1.0);
+        padding: 6px 10px;
+        border-radius: 8px;
+        color: white;
+        border: 1px solid white;
+        box-shadow: 0 0 6px rgba(255, 255, 255, 0.4);
+        z-index: 100;
+    }}
+        .credit a {{
+        color: black;
+        text-decoration: underline;
+    }}
+    </style>
+
+    <div class="credit">
+        Foto de <a href="https://www.pexels.com/es-es/@polina-tankilevitch/" target="_blank">Polina Tankilevitch</a> en <a href="https://www.pexels.com/es-es/" target="_blank">Pexels</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title ("Explorador de videojuegos desde 1980 hasta 2020")
 
 df = pd.read_csv('Data/video_games_sales_completo.csv')
